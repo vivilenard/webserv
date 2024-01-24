80b8763672650c095098138a57b5bc4875ad4a61
Alessandro Paghera <apaghera@2-F-3.42heilbronn.de>
2024-01-24 20:44:26 +0100

    add error handling for address format

--- parse/configFile.hpp
+++ parse/configFile.hpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <cstdlib>
 #include <map>
 
 struct configServer
--- parse/configUtil.cpp
+++ parse/configUtil.cpp
@@ -1,19 +1,73 @@
 #include "configFile.hpp"
 
+bool dotCheck(int *dot, std::string address, int idx)
+{
+	if (address[idx] == '.')
+		(*dot)++;
+	if (address[idx] == '.' && address[idx + 1] && address[idx + 1] == '.')
+	{
+		std::cout <<"Invalid format "<< address << " " << "--> " << address[idx] << " <--" <<std::endl;
+		return (false);
+	}
+	else if (*dot > 3)
+	{
+		std::cout <<"Invalid format "<< address << " " << "--> " << address[idx] << " <--" <<std::endl;
+		return (false);
+	}
+	return (true);
+}
+
+bool	checkAddress(std::string address)
+{
+	int i;
+	int dot;
+
+	i = 0;
+	dot = 0;
+	if (address[i] == '.')
+	{
+		std::cout <<"Invalid format "<< address << " " << "--> " << address[i] << " <--" <<std::endl;
+		return (false);
+	}
+	while (address[i])
+	{
+		if (isdigit(address[i]) || address[i] == '.')
+		{
+			if (!dotCheck(&dot, address, i))
+				return (false);
+		}
+		else
+		{
+			std::cout << "Invalid format " << address << " " << "--> " << address[i] << " <--" << std::endl;
+			return  (false);
+		}
+		i++;
+	}
+	if (dot != 3)
+	{
+		std::cout << "Invalid format " << address << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
 void	ConfigFile::addAddress(configServer &server, std::istringstream &find)
 {
 	std::string tmp;
 	std::string address;
-
+	char* endptr;
 	if (find >> tmp)
 	{
 		std::istringstream iss(tmp);
 		getline(iss,address, ':');
 		std::cout << address << std::endl;
 		server._address = address;
+		if (!checkAddress(address))
+			return ;
 		getline(iss,address, ':');
-		server._listen = std::stoi(address);
+		server._listen = std::strtol(address.c_str(), &endptr, 10);
 		std::cout << server._listen << std::endl;
 	}
 	else
+		std::cerr << "invalid format" << std::endl;
 }
